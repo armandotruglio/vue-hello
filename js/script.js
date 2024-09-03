3d7aa7eccdaa17217e0c4718576cf8e2bdb87a99
armandotruglio <armando.truglio1@gmail.com>
2024-09-03 17:06:54 +0200

    Completato Bonus

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const createApp = Vue.createApp;
 createApp({
     data() {
         return {
-            message: 'Hello World!'
+            message: 'Hello World!',
+            img : 'img/vue.png'
         }
     }
 }).mount('#app');
